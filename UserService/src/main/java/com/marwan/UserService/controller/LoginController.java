b04e4f4f3bfaf0308372206c8c1a6b604e734eeb
marwanBadr14 <marwan.badr@gizasystems.onmicrosoft.com>
2023-11-01 12:33:00 +0200

    Login functionality added. Config still in UserService.

---
+++ UserService/src/main/java/com/marwan/UserService/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.marwan.UserService.controller;
+
+import com.marwan.UserService.reqres.AuthenticationRequest;
+import com.marwan.UserService.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    // expose endpoint for "login" functionality
+    // returns a JWT token
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) throws IllegalAccessException {
+        return ResponseEntity.ok(loginService.authenticate(request));
+    }
+}
+
---
+++ UserService/src/main/java/com/marwan/UserService/service/LoginService.java
@@ -0,0 +1,56 @@
+package com.marwan.UserService.service;
+
+import com.marwan.UserService.helper.UserMapConvertor;
+import com.marwan.UserService.repository.UserRepository;
+import com.marwan.UserService.reqres.AuthenticationRequest;
+import com.marwan.UserService.reqres.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager manager;
+
+    private final UserMapConvertor userMapConvertor = new UserMapConvertor();
+
+
+
+    public Object authenticate(AuthenticationRequest request) throws IllegalAccessException {
+
+        // authenticates that username and password from HTTP request body found in DB
+        manager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        // retrieves the user from the database or returns an error if not found
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        // create a map to generate JWT token
+        Map<String, Object> claims = userMapConvertor.convertUserToMap(user);
+
+        // generates an encoded JWT token for the created user with extra claims
+        var jwtToken = jwtService.generateToken(claims, user);
+
+        // generates an encoded JWT token for the authenticated user
+        // var jwtToken = jwtService.generateToken(user);
+
+        // return the JWT token
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+
+}
