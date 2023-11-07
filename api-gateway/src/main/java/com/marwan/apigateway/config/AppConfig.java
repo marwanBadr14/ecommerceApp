6378a4cf67717cec1173d243bb57ab4d2e14f7ef
Shehab Ihab <shehab.ihab@gizasystems.local>
2023-11-07 14:00:28 +0200

    gateway works with security

--- api-gateway/src/main/java/com/marwan/apigateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/marwan/apigateway/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.marwan.apigateway.config;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,10 @@ public class AppConfig {
     @Bean
     public RestTemplate template(){
        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
     }
 }
--- purchasing-service/src/main/java/com/gizasystems/purchasingservice/PurchasingServiceApplication.java
+++ purchasing-service/src/main/java/com/gizasystems/purchasingservice/PurchasingServiceApplication.java
@@ -2,8 +2,10 @@ package com.gizasystems.purchasingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PurchasingServiceApplication {
 
 	public static void main(String[] args) {
