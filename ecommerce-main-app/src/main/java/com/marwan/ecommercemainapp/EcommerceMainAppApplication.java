2e40ca0a470d55ad1865b1b05d98302792cd2969
marwanBadr14 <marwan.badr@gizasystems.onmicrosoft.com>
2024-02-18 15:10:26 +0200

    Main app integration added

---
+++ ecommerce-main-app/src/main/java/com/marwan/ecommercemainapp/EcommerceMainAppApplication.java
@@ -0,0 +1,13 @@
+package com.marwan.ecommercemainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMainAppApplication.class, args);
+	}
+
+}
